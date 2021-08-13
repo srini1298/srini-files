8bdc456db98a388a3b9c5aaeece32e7063340dbb
Srinivasan D <88872952+srini1298@users.noreply.github.com>
2021-08-13 17:30:59 +0530

    update exercise 1

--- oddnum.js
+++ oddnum.js
@@ -1,8 +1,12 @@
 function show(){
     console.log("odd numbers are:");
-    for(let i=1;i<20;i+=2){
-        
-        console.log(i);
-    }
+    for(let i=0;i<20;i++){
+        if(i%2!=0){
+            console.log(i);
+
+        }
+        else
+        return i;
+    }   
 }
 show();
